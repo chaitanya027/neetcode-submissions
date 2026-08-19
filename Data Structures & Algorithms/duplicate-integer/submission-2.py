9981d110c7054f82802e9e5adf035597e15d4fcb
chaitanya027 <chaitanyanatani27@gmail.com>
2026-08-19 22:48:00 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        bucket = set()
+        for num in nums:
+            if num in bucket:
+                return True
+            bucket.add(num)
+        return False
---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = {}
+        if len(s) != len(t):
+            return False
+        for i in range(0,len(s)):
+            count[s[i]] = count.get(s[i], 0) + 1
+            count[t[i]] = count.get(t[i], 0) - 1
+        
+        for val in count.values():
+            if val != 0:
+                return False
+        return True
